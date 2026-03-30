7aeea91b112cb144317f8b198720fe319dc9baa2
yungProg <122945393+yungProg@users.noreply.github.com>
2026-03-30 21:53:44 +0000

    utilize webpack merge

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,37 +1,7 @@
-import path from "node:path"
-import HtmlWebpackPlugin from "html-webpack-plugin";
+ import { merge } from 'webpack-merge';
+ import common from './webpack.common.js';
 
-export default {
-    mode: "production",
-    entry: "./src/index.js",
-    output: {
-        filename: "main.js",
-        path: path.resolve(import.meta.dirname, "dist"),
-        clean: true,
-    },
-    devtool: "eval-source-map",
-    devServer: {
-        watchFiles: ["./src/template.html"],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/template.html",
-        }),
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
-            },
-            {
-                test: /\.html$/i,
-                use: ["html-loader"],
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: "asset/resource",
-            },
-        ],
-    },
-};
+ export default merge(common, {
+   mode: 'production',
+   devtool: 'source-map',
+ });
