3d3c285199a142f6fdb630e19007e94a3894430a
yungProg <122945393+yungProg@users.noreply.github.com>
2026-03-30 22:03:43 +0000

    add template to html plugin and module object

--- webpack.common.js
+++ webpack.common.js
@@ -12,6 +12,7 @@
    plugins: [
      new HtmlWebpackPlugin({
        title: 'Production',
+       template: "./src/template.html"
      }),
    ],
    output: {
@@ -19,4 +20,20 @@
      path: path.resolve(__dirname, 'dist'),
      clean: true,
    },
+   module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.html$/i,
+                use: ["html-loader"],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            },
+        ],
+    },
  };
